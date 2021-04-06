d37469c1159626d73cb0d54eef93ce0aaac17811
Dmity569 <69630724+Dmity569@users.noreply.github.com>
2021-04-06 18:31:32 +0400

    Update main.py
    4

--- main.py
+++ main.py
@@ -61,4 +61,5 @@ def return_sample_page3():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
