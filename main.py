73e55fa30cc85c684e72a2572b2a2f1e643b1c73
Dmity569 <69630724+Dmity569@users.noreply.github.com>
2021-04-06 18:11:10 +0400

    Create main.py
    1

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def ind():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/countdown')
+def countdown():
+    countdown_list = [str(x) for x in range(10, 0, -1)]
+    countdown_list.append('Пуск!')
+    return '</br>'.join(countdown_list)
+
+
+@app.route('/image_sample')
+def image():
+    return f'''<img src="{url_for('static', filename='img/riana.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+@app.route('/sample_page')
+def return_sample_page():
+    return """<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Первая HTML-страница</h1>
+                  </body>
+                </html>"""
+
+
+@app.route('/sample_page1')
+def return_sample_page1():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Первая HTML-страница</h1>
+                  </body>
+                </html>"""
+
+
+@app.route('/creator')
+def return_sample_page3():
+    return """Dmitry"""
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
